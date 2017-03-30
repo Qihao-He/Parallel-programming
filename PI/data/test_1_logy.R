a865efff40d091e820f956bed21bae1c610c81ac
Qihao He <qi.he@maine.edu>
2017-03-30 15:20:16 -0400

    update

--- PI/data/test_1_logy.R
+++ PI/data/test_1_logy.R
@@ -6,7 +6,8 @@ c(1,2,4,8,16,32,64,16) ->batch
 # do the same for the corresponding y-values
 c(3.3e-07,3.1e-07,2.8e-07,2.7e-07,2.6e-07,2.6e-07,2.5e-07,2.6e-07) -> rel_rms_err
 
-pdf('test_1.pdf')
-logy(batch,rel_rms_err) # do a scatterplot of y on x
+pdf('test_1_logy.pdf')
+#PROBLEM: How to do logy plot?
+plot(batch,rel_rms_err,log="rel_rms_err") # do a scatterplot of y on x
 myString <- "Scatter ploted"
 print ( myString)
