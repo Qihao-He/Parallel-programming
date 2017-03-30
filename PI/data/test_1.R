2657051714dff43135f5101e867c6a842fdcabff
Qihao He <qi.he@maine.edu>
2017-03-30 14:47:46 -0400

    update

--- PI/data/test_1.R
+++ PI/data/test_1.R
@@ -2,10 +2,11 @@
 # This will produce a Rplot.pdf file after the running the Rscript, without opening the Rplot.pdf.
 
 # collect the values together, and assign them to a variable called x
-c(1,2,4,8,16,32,64,16,128,256,512,1024,2048,4096,8192) ->batch
+c(1,2,4,8,16,32,64,16) ->batch
 # do the same for the corresponding y-values
-c(3.3e-07,3.1e-07,2.8e-07,2.7e-07,2.6e-072.6e-07,2.5e-07,2.6e-07,0.062,0.062,0.71,0.94,1,1.1,1.1) -> rel_rms_err
-plot(batch , rel_rms_err) # do a scatterplot of y on x
+c(3.3e-07,3.1e-07,2.8e-07,2.7e-07,2.6e-07,2.6e-07,2.5e-07,2.6e-07) -> rel_rms_err
 
+pdf('test_1.pdf')
+plot(batch,rel_rms_err) # do a scatterplot of y on x
 myString <- "Scatter ploted"
 print ( myString)
---
+++ PI/data/test_1_logy.R
@@ -0,0 +1,12 @@
+# plot a scatter 2D graph
+# This will produce a Rplot.pdf file after the running the Rscript, without opening the Rplot.pdf.
+
+# collect the values together, and assign them to a variable called x
+c(1,2,4,8,16,32,64,16) ->batch
+# do the same for the corresponding y-values
+c(3.3e-07,3.1e-07,2.8e-07,2.7e-07,2.6e-07,2.6e-07,2.5e-07,2.6e-07) -> rel_rms_err
+
+pdf('test_1.pdf')
+logy(batch,rel_rms_err) # do a scatterplot of y on x
+myString <- "Scatter ploted"
+print ( myString)
