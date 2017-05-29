b448384d5d87080d52180cdf89c9624a5cccb147
Qihao He <qi.he@maine.edu>
2017-05-29 12:47:13 -0400

    debug

--- MPI/ece574-project/CLUSTER/14digit/14digit_coarse.c
+++ MPI/ece574-project/CLUSTER/14digit/14digit_coarse.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 int main(){
   double p = 3;
-  for (int i = 2 ; i <= 100000 ; i+=4){
+  int i;
+  for (i = 2 ; i <= 100000 ; i+=4){
   p += 4 / (double(i)*(i+1)*(i+2));
   p -= 4 / (double(i+2)*(i+3)*(i+4));
 }
