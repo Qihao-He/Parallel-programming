405c70d8f6947a3c24091cb2bc6ca136502de92e
Qihao He <qi.he@maine.edu>
2017-05-29 12:58:07 -0400

    debug

--- MPI/ece574-project/CLUSTER/14digit/14digit_coarse.c
+++ MPI/ece574-project/CLUSTER/14digit/14digit_coarse.c
@@ -5,8 +5,8 @@ int main(){
   double p = 3;
   int i;
   for (i = 2 ; i <= 100000 ; i+=4){
-    p += 4 / (double(i)*(i+1)*(i+2));
-    p -= 4 / (double(i+2)*(i+3)*(i+4));
+    p += 4 / (double((i)*(i+1)*(i+2)));
+    p -= 4 / (double((i+2)*(i+3)*(i+4)));
   }
 
   printf("pi=%.14f\n", p);
