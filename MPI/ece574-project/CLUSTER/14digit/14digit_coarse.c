ad40459b3bf57d31608d854b7bdd8d959f792501
Qihao He <qi.he@maine.edu>
2017-05-29 14:17:02 -0400

    debug

--- MPI/ece574-project/CLUSTER/14digit/14digit_coarse.c
+++ MPI/ece574-project/CLUSTER/14digit/14digit_coarse.c
@@ -2,13 +2,13 @@
 
 #include <stdio.h>
 int main(){
-  double p = 3;
+  double p=3;
   int i;
-  for (i = 2; i <= 100000; i+=4){
-    p += 4 / (double(i)*(i+1)*(i+2));
-    p -= 4 / (double(i+2)*(i+3)*(i+4));
+  for (i=2; i<=100000; i+=4){
+    p+=4/(double(i)*(i+1)*(i+2));
+    p-=4/(double(i+2)*(i+3)*(i+4));
   }
 
-  printf("pi=%.14f\n", p);
+  printf("pi=%.14f\n", p); //Print 14 digits after decimal point
   return 0;
 }
