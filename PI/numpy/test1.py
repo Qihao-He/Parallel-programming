5a88175b9e7e048629d5426b254c1f1e4489cd44
Qihao He <qi.he@maine.edu>
2017-07-14 10:45:05 -0400

    update timing test1.py

--- PI/numpy/test1.py
+++ PI/numpy/test1.py
@@ -5,6 +5,10 @@ Created on Thu Jul 13 14:45:09 2017
 
 @author: qihaohe
 """
+import time
+
+start = time.time()
+print("hello")
 
 a=1+2+3+4+5+6+7+8+9
 b=([0,1,2,3,4,5,6,7,8,9])
@@ -12,3 +16,6 @@ b=([0,1,2,3,4,5,6,7,8,9])
 
 print "\na sum of 1 to 9 is: ", a
 print "\nb list of array is: ", b
+
+end = time.time()
+print(end - start)
