0bce56e97c6e433019e0dc606a3ba97e8cb72717
Qihao He <qi.he@maine.edu>
2017-07-14 10:57:45 -0400

    comment on test1

--- PI/numpy/test1.py
+++ PI/numpy/test1.py
@@ -5,16 +5,17 @@ Created on Thu Jul 13 14:45:09 2017
 
 @author: qihaohe
 """
+# Time library for performance measure
 import time
-
 start = time.time()
-print("hello")
 
+# Do something
 a=1+2+3+4+5+6+7+8+9
 b=([0,1,2,3,4,5,6,7,8,9])
-
+print("hello")
 print "\na sum of 1 to 9 is: ", a
 print "\nb list of array is: ", b
 
 end = time.time()
+# Print out time.
 print(end - start)
