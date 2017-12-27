606ae86400ea3a379da4a5c3cc1134395021e60f
Qihao He <qi.he@maine.edu>
2017-12-27 16:40:30 -0500

    numpy long array for fft

---
+++ PI/numpy/numpy_fft_long_array.py
@@ -0,0 +1,27 @@
+"""
+Created: 12/27/2017
+Numpy doing FFT
+Author:Qihao He
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft,ifft
+# This is necessary
+
+N = 64 # Number of points
+T = 1/64.0 # Spaceing between points
+# if T is time/distance, 1/T is frequency/wavenumber
+
+x = np.linspace(0,2*np.pi*N*T,N)
+y1 = np.cos(20*x)
+y2 = np.sin(10*x)
+y3 = np.sin(5*x)
+
+y = y1+y2+y3 # Produces a random signal
+
+fy = fft(y) # Finds the FFT
+xf = np.linspace(0.0,1.0/(2.0*T),N/2)
+
+#plt.plot(xf,(2.0/N)*np.abs(fy[0:N/2]))
+# Only half is valid. The other half is replica!
