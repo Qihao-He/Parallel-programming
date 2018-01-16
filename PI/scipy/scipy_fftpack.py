e1b6f4e8863fa84226c471753fb868ba060c2a79
Qihao He <qi.he@maine.edu>
2018-01-16 14:38:51 -0500

    scipy_fftpack.py add time function

--- PI/scipy/scipy_fftpack.py
+++ PI/scipy/scipy_fftpack.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
+"""
+Created: 1/16/2018
+scipy doing FFT
+Author:Qihao He
+"""
 # import libraries
 import numpy as np
 import scipy as sp
 from scipy.fftpack import fft, ifft
+import time
 
-# jobsize x
-x = 256
-print "jobsize of the array:", x
+start = time.time()# Time counter
+
+pause1 = time.time()
+jobsize = raw_input('Please input a job size:')
+pause2 = time.time()
+
+# jobsize = 256
+# print "jobsize of the array:", jobsize
+
 # creat a long double precision array and print datatype
-a = np.linspace(1., 4., x)
+a = np.linspace(1., 4., jobsize)
 print "double float array 'a' datatype:", a.dtype
 
 # change it to a long single precision array and print datatype
@@ -18,3 +30,6 @@ print "single float array 'b' datatype:", b.dtype
 # do a scipy.fftpack.fft()
 c = sp.fftpack.fft(b)
 print "output of FFT single float complex array 'c' datatype:", c.dtype
+
+end = time.time()
+print"Time elapsed:",(end - start - pause2 + pause1) # Print out time.
