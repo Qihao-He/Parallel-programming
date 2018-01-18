82331b96dd1f100f14330c5c6df9f3713794dab8
Qihao He <qi.he@maine.edu>
2018-01-18 11:48:04 -0500

    remove the print out of the datatype of arrays

--- PI/scipy/scipy_fftpack.py
+++ PI/scipy/scipy_fftpack.py
@@ -12,7 +12,7 @@ from scipy.fftpack import fft, ifft
 import time
 
 start = time.time()# Time counter
-print "This is the name of the script: ", sys.argv[0]
+# print "This is the name of the script: ", sys.argv[0]
 print "The jobsize for the FFT is: ", sys.argv[1]
 # print "Number of arguments: ", len(sys.argv)
 # print "The arguments are: " , str(sys.argv)
@@ -20,15 +20,15 @@ jobsize = sys.argv[1]
 
 # creat a long double precision array and print datatype
 a = np.linspace(1., 4., jobsize)
-print "double float array 'a' datatype:", a.dtype
+# print "double float array 'a' datatype:", a.dtype
 
 # change it to a long single precision array and print datatype
 b = np.float32(a)
-print "single float array 'b' datatype:", b.dtype
+# print "single float array 'b' datatype:", b.dtype
 
 # do a scipy.fftpack.fft()
 c = sp.fftpack.fft(b)
-print "output of FFT single float complex array 'c' datatype:", c.dtype
+# print "output of FFT single float complex array 'c' datatype:", c.dtype
 
 end = time.time()
 print"Time elapsed (Without user input):",(end - start) # Print out time.
