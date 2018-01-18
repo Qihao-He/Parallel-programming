1d50073dd65486e5cd3e62c58fae17ec42c00f8d
Qihao He <qi.he@maine.edu>
2018-01-18 11:58:10 -0500

    update the print jobsize for the scipy-FFT

--- PI/scipy/scipy_fftpack.py
+++ PI/scipy/scipy_fftpack.py
@@ -13,7 +13,7 @@ import time
 
 start = time.time()# Time counter
 # print "This is the name of the script: ", sys.argv[0]
-print "The jobsize for the FFT is: ", sys.argv[1]
+print "The jobsize for the scipy-FFT is: ", sys.argv[1]
 # print "Number of arguments: ", len(sys.argv)
 # print "The arguments are: " , str(sys.argv)
 jobsize = sys.argv[1]
