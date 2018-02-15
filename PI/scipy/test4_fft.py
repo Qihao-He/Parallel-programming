9ba010ce2e78cda766e8eb633fdb8090631e263d
Qihao He <qi.he@maine.edu>
2018-02-15 14:31:20 -0500

    test4_fft.py update

--- PI/scipy/test4_fft.py
+++ PI/scipy/test4_fft.py
@@ -20,7 +20,9 @@ import time
 Usage = """Usage: hello_scipy_fft.py log2_N [log2_M [loops]]
         log2_N = log2(FFT_length),       log2_N = 1...30
         log2_M = log2(FFT_length),       log2_M >= log2_N
-        loops  = number of test repeats, loops>0,       default 1"""
+        loops  = number of test repeats, loops>0,       default 1
+        REL_RMS_ERR_condition = True(1), False(0),    default 0
+        time_elapsed_condition = True(1), False(0),   defalt 0"""
 
 # Default values for optional arguments
 log2_N = 8 #default value to be 8
@@ -38,7 +40,15 @@ loops = 1 #default value to be 1
 if len(sys.argv) > 3:
     loops = int(sys.argv[3])
 
-if (len(sys.argv) < 2 or len(sys.argv) > 4 or loops < 1):
+REL_RMS_ERR_condition = 0
+if len(sys.argv) > 4:
+    REL_RMS_ERR_condition = 1
+
+time_elapsed_condition = 0
+if len(sys.argv) > 5:
+    time_elapsed_condition = 1
+
+if (len(sys.argv) < 2 or len(sys.argv) > 6 or loops < 1):
     print(Usage)
     sys.exit()
 print "The jobsize for the scipy-FFT is 2^", log2_N
@@ -47,8 +57,10 @@ print "Repeat times:", loops
 
 # array of the log2_FFT_length
 log2_FFT_length = np.zeros(log2_M - log2_N, dtype = np.int) # 1D array
-REL_RMS_ERR = np.zeros((loops, log2_M - log2_N), dtype = np.float64) # 2D array
-time_elapsed = np.zeros((loops, log2_M - log2_N), dtype = np.float64) # 2D array
+if REL_RMS_ERR_condition == 1:
+    REL_RMS_ERR = np.zeros((loops, log2_M - log2_N), dtype = np.float64) # 2D array
+if time_elapsed_condition == 1:
+    time_elapsed = np.zeros((loops, log2_M - log2_N), dtype = np.float64) # 2D array
 
 for i in range(0, log2_M - log2_N):
     log2_FFT_length[i] = i + log2_N
@@ -69,41 +81,48 @@ for k in range(loops):
         y = fft(x)
 
         # output buffer and rel_rms_err
-        for i in range(N):
-            re = np.cos(2 * math.pi * i / N) # True solution
-            tsq[0] += pow(re, 2)
-            tsq[1] += pow(re - y.real[i], 2) + pow(y.imag[i], 2)
-        REL_RMS_ERR[k][j] = math.sqrt(tsq[1] / tsq[0])
+        if REL_RMS_ERR_condition == 1:
+            for i in range(N):
+                re = np.cos(2 * math.pi * i / N) # True solution
+                tsq[0] += pow(re, 2)
+                tsq[1] += pow(re - y.real[i], 2) + pow(y.imag[i], 2)
+            REL_RMS_ERR[k][j] = math.sqrt(tsq[1] / tsq[0])
 
         end = time.time()
-        time_elapsed[k][j] = end -start
+        if time_elapsed_condition == 1:
+            time_elapsed[k][j] = end -start
     # print"repeat %i,rel_rms_err = " %k, REL_RMS_ERR[k][:]
-print"rel_rms_err = ", REL_RMS_ERR
-print"time_elapsed = ", time_elapsed
+if REL_RMS_ERR_condition == 1:
+    print"rel_rms_err = ", REL_RMS_ERR
+if time_elapsed_condition == 1:
+    print"time_elapsed = ", time_elapsed
 
 # plot figures
-plt.figure(1)
-plt.subplot(211)
-plt.title('REL_RMS_ERR repeat:%i' %loops)
-for k in range(loops):
-    plt.scatter(log2_FFT_length, REL_RMS_ERR[k, :], c ='b', marker ='o')
-plt.autoscale(enable=True, axis='both', tight=None)
-plt.xlabel('log2_FFT_length: log2_N')
-plt.ylim(1e-07, 3e-07)
-plt.yscale('symlog')
-plt.ylabel('symetric log scale base epsilon')
-plt.grid(True)
-
-
-plt.subplot(212)
-plt.title('time elapsed repeat:%i' %loops)
-for k in range(loops):
-    plt.scatter(log2_FFT_length, time_elapsed[k, :], c ='r', marker ='+')
-plt.autoscale(enable=True, axis='both', tight=None)
-plt.xlabel('log2_FFT_length: log2_N')
-plt.yscale('log')
-plt.ylabel('log scale')
-plt.grid(True)
-
-plt.show()
+if (REL_RMS_ERR_condition == 1 or time_elapsed_condition == 1):
+    plt.figure(1)
+    if REL_RMS_ERR_condition == 1:
+        if (REL_RMS_ERR_condition == 1 and time_elapsed_condition == 1):
+            plt.subplot(211)
+        plt.title('REL_RMS_ERR repeat:%i' %loops)
+        for k in range(loops):
+            plt.scatter(log2_FFT_length, REL_RMS_ERR[k, :], c ='b', marker ='o')
+        plt.autoscale(enable=True, axis='both', tight=None)
+        plt.xlabel('log2_FFT_length: log2_N')
+        plt.ylim(1e-07, 3e-07)
+        plt.yscale('symlog')
+        plt.ylabel('symetric log scale base epsilon')
+        plt.grid(True)
+    if time_elapsed_condition == 1:
+        if (REL_RMS_ERR_condition == 1 and time_elapsed_condition == 1):
+            plt.subplot(212)
+        plt.title('time elapsed repeat:%i' %loops)
+        for k in range(loops):
+            plt.scatter(log2_FFT_length, time_elapsed[k, :], c ='r', marker ='+')
+        # plt.autoscale(enable=True, axis='both', tight=None)
+        plt.ylim(1e-05, 1)
+        plt.xlabel('log2_FFT_length: log2_N')
+        plt.yscale('log')
+        plt.ylabel('log scale')
+        plt.grid(True)
+    plt.show()
 sys.exit()
