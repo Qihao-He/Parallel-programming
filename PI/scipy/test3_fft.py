922fc1ce365f497686a218d8e600f4f17effd5bc
Qihao He <qi.he@maine.edu>
2018-02-14 22:34:15 -0500

    test3_fft.py

--- PI/scipy/test3_fft.py
+++ PI/scipy/test3_fft.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+"""
+Created: 2/14/2018
+desciption:
+scipy doing FFT and generate the same input array and calculate the RMS
+Author:Qihao He
+"""
+# import libraries
 import sys
 import numpy as np
 import scipy as sp
@@ -12,7 +19,7 @@ import time
 # Usage
 Usage = """Usage: hello_scipy_fft.py log2_N [log2_M [loops]]
         log2_N = log2(FFT_length),       log2_N = 1...30
-        log2_M = log2(FFT_length),       log2_M =>log2_N
+        log2_M = log2(FFT_length),       log2_M >= log2_N
         loops  = number of test repeats, loops>0,       default 1"""
 
 # Default values for optional arguments
@@ -20,8 +27,11 @@ log2_N = 8 #default value to be 8
 if len(sys.argv) > 1:
     log2_N = int(sys.argv[1])
 
-log2_M = log2_N + 1 #default value to be 8
+log2_M = log2_N + 1 #default value to be 9
 if len(sys.argv) > 2:
+    if int(sys.argv[2]) <= int(sys.argv[1]):
+        print(Usage)
+        sys.exit()
     log2_M = int(sys.argv[2])
 
 loops = 1 #default value to be 1
@@ -36,47 +46,47 @@ print "The jobsize_end for the scipy-FFT is 2^", log2_M
 print "Repeat times:", loops
 
 # array of the FFT_length
-FFT_length = np.zeros(log2_M - log2_N, )
-REL_RMS_ERR = np.zeros(log2_M - log2_N, )
-duration = np.zeros(log2_M - log2_N, )
+FFT_length = np.zeros(log2_M - log2_N, dtype = np.int)
+REL_RMS_ERR = np.zeros((log2_M - log2_N, loops), dtype = np.float64)
+duration = np.zeros((log2_M - log2_N, loops), dtype = np.float64)
 
 for i in range(0, log2_M - log2_N):
     FFT_length[i] = i + log2_N
 # print "FFT_length:", FFT_length
+for k in range(loops):
+    for j in range(log2_M - log2_N):
+        start = time.time()# Time counter
 
-for j in range(log2_M - log2_N):
-    start = time.time()# Time counter
+        N = 1 << int(FFT_length[j]) #fft length
+        xf = np.linspace(0, 1, N)
 
-    N = 1 << int(FFT_length[j]) #fft length
-    xf = np.linspace(0, 1, N)
+        # input buffer
+        x =  np.zeros((N, ), dtype = np.float64)
+        tsq = np.zeros((2, 1), dtype = np.float64)
+        for i in range(N):
+            x[i] = np.cos(2 * math.pi * i / N)
+            tsq[0] += pow(x[i], 2)
 
-    # input buffer
-    x =  np.zeros((N, ), dtype = np.float64)
-    tsq = np.zeros((2, 1), dtype = np.float64)
-    for i in range(N):
-        x[i] = np.cos(2 * math.pi * i / N)
-        tsq[0] += pow(x[i], 2)
+        # fft and ifft
+        y = fft(x)
+        yinv = ifft(y)
 
-    # fft and ifft
-    y = fft(x)
-    yinv = ifft(y)
+        # output buffer
+        for i in range(N):
+            tsq[1] += pow(x[i] - yinv.real[i], 2) + pow(yinv.imag[i], 2)
+        # rel_rms_err
+        REL_RMS_ERR[j][k] = math.sqrt(tsq[1] / tsq[0])
+        # print"rel_rms_err = ", REL_RMS_ERR[j]
 
-    # output buffer
-    for i in range(N):
-        tsq[1] += pow(x[i] - yinv.real[i], 2) + pow(yinv.imag[i], 2)
-    # rel_rms_err
-    REL_RMS_ERR[j] = math.sqrt(tsq[1] / tsq[0])
-    # print"rel_rms_err = ", REL_RMS_ERR[j]
-
-    end = time.time()
-    duration[j] = end -start
-
+        end = time.time()
+        duration[j][k] = end -start
 
 # plot figures
 plt.figure(1)
 plt.subplot(211)
-plt.title('rel_rms_err')
-plt.scatter(FFT_length, REL_RMS_ERR, c ='b', marker ='.')
+plt.title('REL_RMS_ERR')
+for k in range(loops):
+    plt.scatter(FFT_length, REL_RMS_ERR[:, k], c ='b', marker ='.')
 plt.yscale('symlog')
 plt.ylabel('symetric log scale')
 plt.grid(True)
@@ -85,22 +95,10 @@ plt.grid(True)
 # plt.figure(2)
 plt.subplot(212)
 plt.title('time')
-plt.scatter(FFT_length, duration, c ='r', marker ='+')
+for k in range(loops):
+    plt.scatter(FFT_length, duration[:, k], c ='r', marker ='+')
 plt.yscale('log')
 plt.ylabel('log scale')
 plt.grid(True)
 # axes.Axes.autoscale(enable = True, axis = 'both')
 plt.show()
-
-
-# plt.subplot(311)
-# plt.scatter(xf, x, c ='b', marker ='.')
-# plt.ylabel('FFT Input time domain')
-#
-# plt.subplot(312)
-# plt.scatter(xf, y,  c ='r', marker ='+')
-# plt.ylabel('FFT output Frequency domain')
-#
-# plt.subplot(313)
-# plt.scatter(xf, yinv,  c ='g', marker ='o')
-# plt.ylabel('IFFT output time domain')
