330ef13b1c75c7cf6dc2eb51cb55217e1b0a5cca
Qihao He <qi.he@maine.edu>
2018-03-01 14:09:40 -0500

    t1 2d debug

--- PI/scipy/test1_2d_fft.py
+++ PI/scipy/test1_2d_fft.py
@@ -55,7 +55,7 @@ for l in range(span_N):
         t0 = time.time()# Time counter
         # input buffer
         x = np.zeros((N, N), dtype = np.complex64)
-        print "refcount:%i",sys.getrefcount(x)
+        print "refcount:",sys.getrefcount(x)
         x.real[0,0] = np.float32(1)
 
         # fft execute
